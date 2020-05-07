b720d8cab10e679ca24f902f0828b0ad7395c716
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-05-07 17:04:21 +0700

    a3 adding top nav bar

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,23 @@
-/* Insert your javascript here */
+/*Topnav bar */
+let NavLinks = document.querySelectorAll("nav ul li a");
+let Sections = document.querySelectorAll("section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  NavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
