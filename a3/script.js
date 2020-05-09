f295cdf422587b7cdbf3a2ec0d45505ddc3dc2c6
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-05-09 18:31:34 +0700

    replace()

--- a3/script.js
+++ a3/script.js
@@ -28,6 +28,8 @@ var date1 = new Date("2020-05-06T16:00");
 document.getElementById("button1").innerHTML = date1;
 */
 
+
+/* 
 function movieSchdule() {
   var now = new Date();
   var dateLimit = new Date("2020-06-10T15:00:00")
@@ -40,3 +42,5 @@ function adultNum(){
 var 
 
 }
+
+*/
